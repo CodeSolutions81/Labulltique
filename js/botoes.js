76300e4340f791461d38312ddd1af35bf69557f9
Pedro Nardelli <pedronardelli@Pedros-MacBook-Pro.local>
2022-10-26 23:05:45 -0300

    inserido botoes do menu com funcionalidades das comidas

---
+++ js/botoes.js
@@ -0,0 +1,7 @@
+function alterarCarnes(){
+       console.log(document.getElementById("imagem-principal").src="/img/Carnes.jpg")
+}
+
+function alterarEspetinho(){
+    console.log(document.getElementById("imagem-principal").src="/img/Espetos.jpg")
+}
