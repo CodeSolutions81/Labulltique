4f0c52f5186121c83916d2957f28531cf142ccc3
Pedro Nardelli <pedronardelli@Pedros-MacBook-Pro.local>
2022-10-31 14:58:02 -0300

    adicionado as bebidas e destilados com funcionalidade

--- js/botoes.js
+++ js/botoes.js
@@ -5,3 +5,11 @@ function alterarCarnes(){
 function alterarEspetinho(){
     console.log(document.getElementById("imagem-principal").src="/img/Espetos.jpg")
 }
+
+function alterarWhiskey(){
+    console.log(document.getElementById("imagem-principal").src="/img/Whiskey.jpg")
+}
+
+function alterarDestilados(){
+ console.log(document.getElementById("imagem-principal").src="/img/Destilados-Drink.jpg")
+}
